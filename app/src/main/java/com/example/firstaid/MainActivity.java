271206abab062c3c9871364ced7fd9009adfbdfe
JasmanShah <jasmanshah@gmail.com>
2021-07-06 15:41:09 +0800

    list view with global text

--- app/src/main/java/com/example/firstaid/MainActivity.java
+++ app/src/main/java/com/example/firstaid/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.firstaid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class Global {
+        public static String items[], items2 [];
+    }
+
+    public void onListClick(View v) {
+        if (v.getId() == R.id.venom) {
+            Global.items = new String[]{"Snake","Scorpion","Spider"};
+            Global.items2 = new String[]{"Snake bite","Scorpion sting","Spider bite"};
+            Intent i = new Intent(MainActivity.this,listview_text.class);
+            startActivity(i);
+        }
+
+        if (v.getId() == R.id.poison) {
+            Global.items = new String[]{"Chemical","Food"};
+            Global.items2 = new String[]{"Chemical Poisoning","Food Poisoning"};
+            Intent i = new Intent(MainActivity.this,listview_text.class);
+            startActivity(i);
+        }
+
+        if (v.getId() == R.id.skin) {
+            Global.items = new String[]{"Burn","Rash"};
+            Global.items2 = new String[]{"1st degree burn","Poison Ivy"};
+            Intent i = new Intent(MainActivity.this,listview_text.class);
+            startActivity(i);
+        }
+
+        if (v.getId() == R.id.firstaidkit) {
+            Global.items = new String[]{"Chemical","Food"};
+            Global.items2 = new String[]{"Chemical Poisoning","Food Poisoning"};
+            Intent i = new Intent(MainActivity.this,listview_text.class);
+            startActivity(i);
+        }
+
+        if (v.getId() == R.id.emergencycall) {
+            Global.items = new String[]{"Perlis","Kedah"};
+            Global.items2 = new String[]{"Perlis emergency number","Not in list yet"};
+            Intent i = new Intent(MainActivity.this,listview_text.class);
+            startActivity(i);
+        }
     }
 }
---
+++ app/src/main/java/com/example/firstaid/listview_text.java
@@ -0,0 +1,31 @@
+package com.example.firstaid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class listview_text extends Activity {
+//    String items [] = {"LAN2","MAN","WAN"};
+//    String items2 []=new String[]{"LOCAL AREA NETWORK","METROPOLITAN AREA NETWORK","WIDE AREA NETWORK"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview_text);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MainActivity.Global.items);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(listview_text.this, MainActivity.Global.items2[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
