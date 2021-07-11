52f1f1d58e29d18f989ae00a3a52cad935675f80
JasmanShah <jasmanshah@gmail.com>
2021-07-11 15:09:03 +0800

    Personal Info Layout design

--- app/src/main/java/com/example/firstaid/personal_info.java
+++ app/src/main/java/com/example/firstaid/personal_info.java
@@ -1,6 +1,8 @@
 package com.example.firstaid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +10,10 @@ public class personal_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_info_layout);
+        Button deleteButton = findViewById(R.id.deleteInfo);
+        Button updateButton = findViewById(R.id.updateInfo);
+        Button insertButton = findViewById(R.id.insertInfo);
+        deleteButton.setVisibility(View.INVISIBLE);
+        updateButton.setVisibility(View.INVISIBLE);
     }
 }
